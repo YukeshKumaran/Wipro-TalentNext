86581b53ddc58d652675b20e1eade9bd1afacb6e
YukeshKumaran <162685072+YukeshKumaran@users.noreply.github.com>
2025-07-19 08:56:26 +0530

    Create AddTwoInteger.java

---
+++ AddTwoInteger.java
@@ -0,0 +1,8 @@
+class AddTwoInteger{
+    public static void main(String[] args) {
+        int i1 = Integer.parseInt(args[0]);
+        int i2 = Integer.parseInt(args[1]);
+        int total = i1+i2;
+        System.out.print("the sum of "+args[0]+" and "+args[1]+" is "+total); 
+    }
+}
